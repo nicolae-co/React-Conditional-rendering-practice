93bc26db420bf69cc1a10d90511df08c4ec0fde0
Nicolae <106536189+nicolae-co@users.noreply.github.com>
2023-01-25 19:05:47 +0200

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,7 +6,11 @@ function App() {
  
   return (
       <div>
-          {messages.length !== 0 && <h1>You have {messages.length} unread messages!</h1>}
+          {
+            messages.length === 0 ? 
+            <h1>You're all caught up!</h1> : 
+            <h1>You have {messages.length} unread {messages.length > 1 ? "messages" : "message"}!</h1>
+          }
       </div>
   )
 }
